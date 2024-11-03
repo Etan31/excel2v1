4b8cd994491fae426d338bbb6efd35823e0f9e12
Tristan Ehron Tumbaga <tumbagatristanehron31@gmail.com>
2024-11-03 10:21:06 +0800

    optimized UI

---
+++ public/js/dataManagement.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetchHeaders('headersRow', 'subheadersRow'); // For Data Management Table
+    fetchHeaders('columnHeadersRow', 'columnSubheadersRow'); // For Column Management Table
+});
